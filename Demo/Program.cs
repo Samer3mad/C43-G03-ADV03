24ab52441dc73b065d5ba2063733ad00231c9dfc
Samer Zaki <Samer.Zaki22@svnu.edu.eg>
2025-01-25 13:58:00 +0200

    Delegate Example 01

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -2,6 +2,35 @@
 {
     internal class Program
     {
+    class StringFun
+    {
+        public static int GetCountOfUpperCaseChars(string Name)
+        {
+            int count = 0;
+            if (Name is not null)
+            {
+                for (int i = 0; i < Name.Length; i++)
+                    if (char.IsUpper(Name[i]))
+                        count++;
+            }
+            return count;
+        }
+
+        public static int GetCountOfLowerCaseChars(string Name)
+        {
+            int count = 0;
+            if (Name is not null)
+            {
+                for (int i = 0; i < Name.Length; i++)
+                    if (char.IsLower(Name[i]))
+                        count++;
+            }
+            return count;
+        }
+    }
+        ///Step 0. Declaring Delegate Function
+        public delegate int CustomFunc(string arg01);
+
         static void Main(string[] args)
         {
             #region What is Delegate
@@ -11,6 +40,21 @@
             ///Delegates has 2 Usages:-
             ///1. Functional Programming 
             ///2. Event-Driven Programming
+            #endregion
+
+            #region Delegate Example 01
+
+            ///Step 1. Declare delegate reference 
+            ///CustomFunc reference;
+
+            ///Step 2. Initialize Delegate reference 
+            //reference = new CustomFunc(StringFun.GetCountOfUpperCaseChars);
+            ///reference = StringFun.GetCountOfUpperCaseChars; //"Syntax Sugar"
+            //reference = StringFun.GetCountOfLowerCaseChars;
+
+            //Step 3. Use The Delegate reference
+            ///int Res = reference("SaMer EMAd");
+            ///Console.WriteLine($"Result = {Res}");
             #endregion
         }
     }
